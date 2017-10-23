d0dc256090843d9bfd02c11949fff2c67a9b8dbd
申行 <binary2015nov@outlook.com>
2017-10-23 10:09:39 +0800

    fix Backbone.Todos

--- src/Backbone.Todos/Global.asax.cs
+++ src/Backbone.Todos/Global.asax.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 
-//The entire C# source code for the ServiceStack + Redis TODO REST backend. There is no other .cs :)
 namespace Backbone.Todos
-{
+{  
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
         {
             //Initialize your ServiceStack AppHost
-            new AppHost().Init();
+            new TodosAppHost().Init();
         }
     }
 }
--- src/Backbone.Todos/TodosAppHost.cs
+++ src/Backbone.Todos/TodosAppHost.cs
@@ -1,21 +1,70 @@
-﻿using Funq;
-using ServiceStack;
+﻿using ServiceStack;
 using ServiceStack.Redis;
 
 namespace Backbone.Todos
 {
+    // Define your ServiceStack web service request (i.e. Request DTO).
+    public class Todo
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+        public int Order { get; set; }
+        public bool Done { get; set; }
+    }
+
+    // Create your ServiceStack rest-ful web service implementation. 
+    public class TodoService : Service
+    {
+        public object Get(Todo todo)
+        {
+            //Return a single Todo if the id is provided.
+            if (todo.Id != 0)
+                return Redis.As<Todo>().GetById(todo.Id);
+
+            //Return all Todos items.
+            return Redis.As<Todo>().GetAll();
+        }
+
+        // Handles creating and updating the Todo items.
+        public Todo Post(Todo todo)
+        {
+            var redis = Redis.As<Todo>();
+
+            //Get next id for new todo
+            if (todo.Id == 0)
+                todo.Id = redis.GetNextSequence();
+
+            redis.Store(todo);
+
+            return todo;
+        }
+
+        // Handles creating and updating the Todo items.
+        public Todo Put(Todo todo)
+        {
+            return Post(todo);
+        }
+
+        // Handles Deleting the Todo item
+        public Todo Delete(Todo todo)
+        {
+            Redis.As<Todo>().DeleteById(todo.Id);
+            return todo;
+        }
+    }
+
     // Create your ServiceStack web service application with a singleton AppHost.
-    public class AppHost : AppHostBase
+    public class TodosAppHost : AppHostBase
     {
         // Initializes a new instance of your ServiceStack application, with the specified name and assembly containing the services.
-        public AppHost() : base("Backbone.js TODO", typeof(TodosService).Assembly)
+        public TodosAppHost() : base("Backbone.js TODO", typeof(TodoService).Assembly)
         {
             //Configure ServiceStack Json web services to return idiomatic Json camelCase properties.
             Config.UseCamelCase = true;
         }
 
         // Configure the container with the necessary routes for your ServiceStack application.
-        public override void Configure(Container container)
+        public override void Configure(Funq.Container container)
         {
             //Register Redis factory in Funq IoC. The default port for Redis is 6379.
             container.Register<IRedisClientsManager>(new BasicRedisClientManager("localhost:6379"));
